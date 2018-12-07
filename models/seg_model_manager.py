6c900518ff31c2b8e468786fe9f561e0838315bf
Donny You <18500770638@163.com>
2018-12-07 12:35:04 +0800

    code refactor.

--- models/seg_model_manager.py
+++ models/seg_model_manager.py
@@ -8,15 +8,21 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from models.img.deeplabv3 import DeepLabV3
-from models.img.pspnet import PSPNet
-from models.img.denseassp import DenseASPP
+from models.deeplabv3 import DeepLabV3
+from models.denseassp import DenseASPP
+from models.asp_ocnet import AspOCNet
+from models.asp_ocnetv2 import AspOCNetV2
+from models.base_ocnet import BaseOCNet
+from models.pspnet import PSPNet
 from utils.tools.logger import Logger as Log
 
 SEG_MODEL_DICT = {
     'deeplabv3': DeepLabV3,
     'pspnet': PSPNet,
-    'denseaspp': DenseASPP
+    'denseaspp': DenseASPP,
+    'asp_ocnet': AspOCNet,
+    'asp_ocnetv2': AspOCNetV2,
+    'base_ocnet': BaseOCNet
 }
 
 
