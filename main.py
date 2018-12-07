ac4208d469f910266735604ddfe81cd255cf65b2
Donny You <18500770638@163.com>
2018-12-07 14:36:00 +0800

    add multi grid.

--- main.py
+++ main.py
@@ -63,6 +63,8 @@ if __name__ == "__main__":
                         dest='network:model_name', help='The name of model.')
     parser.add_argument('--backbone', default=None, type=str,
                         dest='network:backbone', help='The base network of model.')
+    parser.add_argument('--multi_grid',  default=[1, 1, 1], nargs='+', type=int,
+                        dest='network:multi_grid', help='The multi_grid for resnet backbone.')
     parser.add_argument('--pretrained', type=str, default=None,
                         dest='network:pretrained', help='The path to pretrained model.')
     parser.add_argument('--resume', default=None, type=str,
