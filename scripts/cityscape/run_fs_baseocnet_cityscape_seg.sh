c67d58cb0f70c639471d477f6280509cb9d5f20b
Donny You <18500770638@163.com>
2018-12-08 12:51:44 +0800

    train aspocnet.

--- scripts/cityscape/run_fs_baseocnet_cityscape_seg.sh
+++ scripts/cityscape/run_fs_baseocnet_cityscape_seg.sh
@@ -6,14 +6,14 @@ PYTHON="/root/miniconda3/bin/python"
 
 cd ../../
 
-STD_LOG_FILE="./log/std_log.log"+"`date +%Y-%m-%d,%H:%m:%s`"
+LOG_FILE="./log/cityscape/fs_baseocnet_cityscape_seg.log"
 
 
 if [ "$1"x == "train"x ]; then
   $PYTHON main.py --hypes hypes/cityscape/fs_baseocnet_cityscape_seg.json \
                   --phase train --gathered n --loss_balance y \
                   --data_dir /teamscratch/msravcshare/v-ansheng/DataSet/CityScape \
-                  --pretrained ./pretrained_model/resnet101-imagenet.pth  > $STD_LOG_FILE 2>&1
+                  --pretrained ./pretrained_model/resnet101-imagenet.pth  > $LOG_FILE 2>&1
 
 elif [ "$1"x == "debug"x ]; then
   $PYTHON main.py --hypes hypes/cityscape/fs_baseocnet_cityscape_seg.json --phase debug --gpu 0
--- scripts/cityscape/run_fs_pspnet_cityscape_seg.sh
+++ scripts/cityscape/run_fs_pspnet_cityscape_seg.sh
@@ -5,19 +5,19 @@ cd ../../
 
 
 if [ "$1"x == "train"x ]; then
-  python main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json \
-                 --phase train --gpu 0 1 2 3 --gathered n --loss_balance y \
-                 --pretrained ./pretrained_model/resnet101-imagenet.pth
+  python -u main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json \
+                    --phase train --gpu 0 1 2 3 --gathered n --loss_balance y \
+                    --pretrained ./pretrained_model/resnet101-imagenet.pth
 
 elif [ "$1"x == "debug"x ]; then
-  python main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json --phase debug --gpu 0
+  python -u main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json --phase debug --gpu 0 \
 
 elif [ "$1"x == "test"x ]; then
-  python main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json --phase test --gpu 0 --resume $2
+  python -u main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json --phase test --gpu 0 --resume $2
   cd val/scripts
-  python cityscape_evaluator.py --hypes_file ../../../hypes/cityscape/fs_pspnet_cityscape_seg.json
-                             --gt_dir path-to-gt
-                             --pred_dir path-to-pred
+  python -u cityscape_evaluator.py --hypes_file ../../../hypes/cityscape/fs_pspnet_cityscape_seg.json \
+                                   --gt_dir path-to-gt \
+                                   --pred_dir path-to-pred
 else
   echo "$1"x" is invalid..."
 fi
