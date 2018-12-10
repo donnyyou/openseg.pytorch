047ae0ed8f23a43d2492e0b6560cae437a9b855c
Donny You <18500770638@163.com>
2018-12-10 13:24:24 +0800

    train pspnet.

--- scripts/cityscape/run_fs_pspnet_cityscape_seg.sh
+++ scripts/cityscape/run_fs_pspnet_cityscape_seg.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 
+# check the enviroment info
+nvidia-smi
+PYTHON="/root/miniconda3/bin/python"
 
 cd ../../
 
+LOG_FILE="./log/cityscape/fs_pspnet_cityscape_seg.log"
+
 
 if [ "$1"x == "train"x ]; then
-  python -u main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json \
-                    --phase train --gpu 0 1 2 3 --gathered n --loss_balance y \
-                    --pretrained ./pretrained_model/resnet101-imagenet.pth
+  $PYTHON main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json \
+                  --phase train --gathered n --loss_balance y \
+                  --data_dir /teamscratch/msravcshare/v-ansheng/DataSet/CityScape \
+                  --pretrained ./pretrained_model/resnet101-imagenet.pth  > $LOG_FILE 2>&1
 
 elif [ "$1"x == "debug"x ]; then
-  python -u main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json --phase debug --gpu 0 \
+  $PYTHON main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json --phase debug --gpu 0
 
 elif [ "$1"x == "test"x ]; then
-  python -u main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json --phase test --gpu 0 --resume $2
+  $PYTHON main.py --hypes hypes/cityscape/fs_pspnet_cityscape_seg.json --phase test --gpu 0 --resume $2
   cd val/scripts
-  python -u cityscape_evaluator.py --hypes_file ../../../hypes/cityscape/fs_pspnet_cityscape_seg.json \
-                                   --gt_dir path-to-gt \
-                                   --pred_dir path-to-pred
+  $PYTHON cityscape_evaluator.py --hypes_file ../../hypes/cityscape/fs_pspnet_cityscape_seg.json \
+                                 --gt_dir path-to-gt \
+                                 --pred_dir path-to-pred
 else
   echo "$1"x" is invalid..."
 fi
