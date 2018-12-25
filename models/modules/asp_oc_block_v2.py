c372ffe0ce66853c70e5fae65ab2d14e6bc3573d
Donny You <18500770638@163.com>
2018-12-25 16:10:12 +0800

    train aspocv5.

--- models/modules/asp_oc_block_v2.py
+++ models/modules/asp_oc_block_v2.py
@@ -11,10 +11,11 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 
 from models.modules.base_oc_block_v2 import BaseOC_Context_Module_v3, BaseOC_Context_Module_v4
-from models.modules.self_attention_module import SelfAttentionModule
+from models.modules.self_attention_module import SelfAttentionModuleV2
 from models.tools.module_helper import ModuleHelper
 
 
@@ -129,42 +130,32 @@ class ASP_OC_Module_v4(nn.Module):
 
 
 class ASP_OC_Module_v5(nn.Module):
-    def __init__(self, features, out_features=512, dilations=(12, 24, 36), bn_type=None):
+    def __init__(self, features, inner_features=256, out_features=512, dilations=(12, 24, 36), bn_type=None):
         super(ASP_OC_Module_v5, self).__init__()
-        self.context = nn.Sequential(nn.Conv2d(features, out_features, kernel_size=3, padding=1, dilation=1, bias=True),
-                                     ModuleHelper.BNReLU(out_features, bn_type=bn_type),
-                                     BaseOC_Context_Module_v4(in_channels=out_features, out_channels=out_features,
-                                                              key_channels=out_features//2, value_channels=out_features//2,
-                                                              dropout=0, sizes=([2]), bn_type=bn_type))
-        self.conv2 = nn.Sequential(nn.Conv2d(features, out_features, kernel_size=1, padding=0, dilation=1, bias=False),
-                                   ModuleHelper.BNReLU(out_features, bn_type=bn_type))
-        self.conv3 = nn.Sequential(SelfAttentionModule(in_channels=features, key_channels=out_features//2,
-                                                       value_channels=out_features//2, out_channels=out_features,
-                                                       kernel_size=5, padding=dilations[0], dilation=dilations[0] // 2,
-                                                       bn_type=bn_type),
-                                   ModuleHelper.BNReLU(out_features, bn_type=bn_type))
-        self.conv4 = nn.Sequential(SelfAttentionModule(in_channels=features, key_channels=out_features//2,
-                                                       value_channels=out_features//2, out_channels=out_features,
-                                                       kernel_size=5, padding=dilations[1], dilation=dilations[1] // 2,
-                                                       bn_type=bn_type),
-                                   ModuleHelper.BNReLU(out_features, bn_type=bn_type))
-        self.conv5 = nn.Sequential(SelfAttentionModule(in_channels=features, key_channels=out_features//2,
-                                                       value_channels=out_features//2, out_channels=out_features,
-                                                       kernel_size=5, padding=dilations[2], dilation=dilations[2] // 2,
-                                                       bn_type=bn_type),
+        self.conv1 = nn.Sequential(nn.AdaptiveAvgPool2d((1, 1)),
+                                   nn.Conv2d(features, inner_features, kernel_size=1, padding=0, dilation=1,
+                                             bias=False),
+                                   ModuleHelper.BNReLU(inner_features, bn_type=bn_type))
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(features, inner_features, kernel_size=1, padding=0, dilation=1, bias=False),
+            ModuleHelper.BNReLU(inner_features, bn_type=bn_type))
+        self.conv3 = nn.Sequential(SelfAttentionModuleV2(in_channels=features, key_channels=out_features//2,
+                                                         value_channels=out_features//2, out_channels=inner_features,
+                                                         kernel_size=3, padding_list=dilations, dilation_list=dilations,
+                                                         bn_type=bn_type),
                                    ModuleHelper.BNReLU(out_features, bn_type=bn_type))
 
         self.conv_bn_dropout = nn.Sequential(
-            nn.Conv2d(out_features * 5, out_features, kernel_size=1, padding=0, dilation=1, bias=False),
+            nn.Conv2d(out_features * 3, out_features, kernel_size=1, padding=0, dilation=1, bias=False),
             ModuleHelper.BNReLU(out_features, bn_type=bn_type),
             nn.Dropout2d(0.1)
             )
 
-    def _cat_each(self, feat1, feat2, feat3, feat4, feat5):
+    def _cat_each(self, feat1, feat2, feat3):
         assert(len(feat1)==len(feat2))
         z = []
         for i in range(len(feat1)):
-            z.append(torch.cat((feat1[i], feat2[i], feat3[i], feat4[i], feat5[i]), 1))
+            z.append(torch.cat((feat1[i], feat2[i], feat3[i]), 1))
         return z
 
     def forward(self, x):
@@ -175,16 +166,14 @@ class ASP_OC_Module_v5(nn.Module):
         else:
             raise RuntimeError('unknown input type')
 
-        feat1 = self.context(x)
+        feat1 = F.interpolate(self.conv1(x), size=(h, w), mode='bilinear', align_corners=True)
         feat2 = self.conv2(x)
         feat3 = self.conv3(x)
-        feat4 = self.conv4(x)
-        feat5 = self.conv5(x)
 
         if isinstance(x, Variable):
-            out = torch.cat((feat1, feat2, feat3, feat4, feat5), 1)
+            out = torch.cat((feat1, feat2, feat3), 1)
         elif isinstance(x, tuple) or isinstance(x, list):
-            out = self._cat_each(feat1, feat2, feat3, feat4, feat5)
+            out = self._cat_each(feat1, feat2, feat3)
         else:
             raise RuntimeError('unknown input type')
 
--- models/modules/self_attention_module.py
+++ models/modules/self_attention_module.py
@@ -101,6 +101,102 @@ class SelfAttentionModule(nn.Module):
         return context
 
 
+class SelfAttentionModuleV2(nn.Module):
+
+    def __init__(self, in_channels, key_channels, value_channels,
+                 out_channels=None, kernel_size=1, dilation_list=None,
+                 padding_list=None, stride=1, scale=1, bn_type=None):
+        super(SelfAttentionModuleV2, self).__init__()
+        self.scale = scale
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.key_channels = key_channels
+        self.value_channels = value_channels
+        self.kernel_size = self._pair(kernel_size)
+        self.padding_list = padding_list
+        self.dilation_list = dilation_list
+        self.stride = self._pair(stride)
+        if out_channels is None:
+            self.out_channels = in_channels
+        self.pool = nn.MaxPool2d(kernel_size=(scale, scale))
+        self.f_key = nn.Sequential(
+            nn.Conv2d(in_channels=self.in_channels, out_channels=self.key_channels,
+                      kernel_size=1, stride=1, padding=0),
+            ModuleHelper.BNReLU(self.key_channels, bn_type=bn_type),
+            nn.Conv2d(in_channels=self.key_channels, out_channels=self.key_channels,
+                      kernel_size=1, stride=1, padding=0),
+            ModuleHelper.BNReLU(self.key_channels, bn_type=bn_type),
+        )
+        self.f_query = nn.Sequential(
+            nn.Conv2d(in_channels=self.in_channels, out_channels=self.key_channels,
+                      kernel_size=1, stride=1, padding=0),
+            ModuleHelper.BNReLU(self.key_channels, bn_type=bn_type),
+            nn.Conv2d(in_channels=self.key_channels, out_channels=self.key_channels,
+                      kernel_size=1, stride=1, padding=0),
+            ModuleHelper.BNReLU(self.key_channels, bn_type=bn_type),
+        )
+        # self.f_query = self.f_key
+        self.f_value = nn.Conv2d(in_channels=self.in_channels, out_channels=self.value_channels,
+                                 kernel_size=1, stride=1, padding=0)
+        self.W = nn.Conv2d(in_channels=self.value_channels, out_channels=self.out_channels,
+                           kernel_size=1, stride=1, padding=0)
+        nn.init.constant_(self.W.weight, 0)
+        nn.init.constant_(self.W.bias, 0)
+
+    @staticmethod
+    def _pair(x):
+        if isinstance(x, (list, tuple)):
+            return x
+        return tuple(repeat(x, 2))
+
+    def forward(self, x):
+        b, _, h, w = x.size()
+        if self.scale > 1:
+            x = self.pool(x)
+
+        value = self.f_value(x)
+        _, value_c, value_h, value_w = value.size()
+        unfold_value_list = []
+        for dilation, padding in zip(self.dilation_list, self.padding_list):
+            unfold_value = F.unfold(value, kernel_size=self.kernel_size,
+                                    dilation=dilation, padding=padding, stride=self.stride)
+            unfold_value_h, unfold_value_w = self._out_size([value_h, value_w])
+            unfold_value = unfold_value.view(b, value_c, -1, unfold_value_h, unfold_value_w).contiguous()
+            # print('unfold_value: {}'.format(unfold_value.size()))
+            unfold_value_list.append(unfold_value)
+
+        unfold_value = torch.cat(unfold_value_list, 2)
+
+        key = self.f_key(x)
+        _, key_c, key_h, key_w = key.size()
+        unfold_key_list = []
+        for dilation, padding in zip(self.dilation_list, self.padding_list):
+            unfold_key = F.unfold(key, kernel_size=self.kernel_size,
+                                  dilation=dilation, padding=padding, stride=self.stride)
+            unfold_key_h, unfold_key_w = self._out_size([key_h, key_w])
+            unfold_key = unfold_key.view(b, key_c, -1, unfold_key_h, unfold_key_w).contiguous()
+            # print('unfold_key: {}'.format(unfold_key.size()))
+            unfold_key_list.append(unfold_key)
+
+        unfold_key = torch.cat(unfold_key_list, 2)
+
+        assert unfold_value.size(-2) == unfold_key.size(-2) and unfold_value.size(-1) == unfold_key.size(-1)
+
+        query = self.f_query(x)
+        query = query.unsqueeze(2)
+        # print('query: {}'.format(query.size()))
+
+        sim_map = (unfold_key * query).sum(1, keepdim=True)
+        sim_map = F.softmax(sim_map, 2)
+        # print('sim_map: {}'.format(sim_map.size()))
+
+        context = (sim_map * unfold_value).sum(2).contiguous()
+        context = self.W(context)
+        context = F.interpolate(input=context, size=(h, w), mode='bilinear', align_corners=True)
+        # print('context: {}'.format(context.size()))
+        return context
+
+
 if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = '0'
     in_x = torch.randn((2, 10, 256, 512)).cuda()
