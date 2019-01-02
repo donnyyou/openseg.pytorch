1ca9d9b65c624c4c9a780be17e3aac21a1112319
Donny You <18500770638@163.com>
2019-01-02 17:41:09 +0800

    fix bug.

--- scripts/ade20k/run_fs_res50_pspnet_ade20k_seg.sh
+++ scripts/ade20k/run_fs_res50_pspnet_ade20k_seg.sh
@@ -12,7 +12,7 @@ DATA_DIR="/msravcshare/v-ansheng/DataSet/ADE20K"
 BACKBONE="deepbase_resnet50_dilated8"
 MODEL_NAME="pspnet"
 LOSS_TYPE="fs_auxce_loss"
-CHECKPOINTS_NAME="fs_res50_pspnet_ade20k_seg"
+CHECKPOINTS_NAME="fs_res50_pspnet_ade20k_seg"$2
 PRETRAINED_MODEL="./pretrained_model/resnet50-imagenet.pth"
 MAX_ITERS=150000
 
