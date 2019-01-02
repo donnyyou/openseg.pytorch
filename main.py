413388ff413b9dfa83fd6c8369720be383ace6ef
Donny You <18500770638@163.com>
2019-01-02 20:21:47 +0800

    fix default value.

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
                         dest='test:test_img', help='The test path of image.')
     parser.add_argument('--test_dir', default=None, type=str,
                         dest='test:test_dir', help='The test directory of images.')
-    parser.add_argument('--out_dir', default='val', type=str,
+    parser.add_argument('--out_dir', default='none', type=str,
                         dest='test:out_dir', help='The test out directory of images.')
 
     args_parser = parser.parse_args()
